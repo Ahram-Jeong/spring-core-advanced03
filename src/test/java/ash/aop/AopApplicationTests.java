8bc75adc42ba4b16c5e3ac8585b811635ed027cd
Ahram-Jeong <ahram1215@gmail.com>
2025-02-01 00:47:50 +0900

    Initial commit;-)

---
+++ src/test/java/ash/aop/AopApplicationTests.java
@@ -0,0 +1,13 @@
+package ash.aop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
