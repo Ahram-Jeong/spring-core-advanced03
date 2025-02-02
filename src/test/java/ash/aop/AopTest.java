da3f2734b56581efc8dbd52d14bd31f226b47629
Ahram-Jeong <ahram1215@gmail.com>
2025-02-03 00:25:33 +0900

    20250203 - 스프링 AOP : 스프링 AOP 구현 1_시작

--- src/test/java/ash/aop/AopTest.java
+++ src/test/java/ash/aop/AopTest.java
@@ -2,15 +2,18 @@ package ash.aop;
 
 import ash.aop.order.OrderRepository;
 import ash.aop.order.OrderService;
+import ash.aop.order.aop.AspectV1;
 import lombok.extern.slf4j.Slf4j;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.aop.support.AopUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
 
 @Slf4j
 @SpringBootTest
+@Import(AspectV1.class) // bean 등록
 public class AopTest {
     @Autowired
     OrderService orderService;
