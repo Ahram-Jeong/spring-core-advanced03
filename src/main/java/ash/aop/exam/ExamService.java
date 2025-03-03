1585fe2be19049fb122c318289ef0f1b73f6e189
Ahram-Jeong <ahram1215@gmail.com>
2025-03-03 19:18:00 +0900

    20250303 - 스프링 AOP 실전 예제 : 로그 출력 AOP

--- src/main/java/ash/aop/exam/ExamService.java
+++ src/main/java/ash/aop/exam/ExamService.java
@@ -1,5 +1,6 @@
 package ash.aop.exam;
 
+import ash.aop.exam.annotation.Trace;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 public class ExamService {
     private final ExamRepository examRepository;
 
+    @Trace
     public void request(String itemId) {
         examRepository.save(itemId);
     }
