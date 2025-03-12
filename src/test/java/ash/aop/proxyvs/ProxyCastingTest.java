b13b860ef26943e921887951027f064a727417d0
Ahram-Jeong <ahram1215@gmail.com>
2025-03-12 21:06:14 +0900

    20250312 - 스프링 AOP 실무 주의사항 : 프록시 기술과 한계 - 타입 캐스팅 3_CGLIB 프록시 캐스팅

--- src/test/java/ash/aop/proxyvs/ProxyCastingTest.java
+++ src/test/java/ash/aop/proxyvs/ProxyCastingTest.java
@@ -13,12 +13,8 @@ public class ProxyCastingTest {
     @Test
     void jdkProxy() {
         MemberServiceImpl target = new MemberServiceImpl();
-        ProxyFactory proxyFactory = new ProxyFactory();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
         proxyFactory.setProxyTargetClass(false); // JDK 동적 프록시
-        proxyFactory.addInterface(MemberService.class);
-
-        // 빈 어드바이스 추가하여 프록시 객체 강제 생성
-        proxyFactory.addAdvice((org.aopalliance.intercept.MethodInterceptor) invocation -> invocation.proceed());
 
         // 프록시를 인터페이스로 캐스팅 성공
 //        Object proxy = proxyFactory.getProxy();
@@ -29,5 +25,21 @@ public class ProxyCastingTest {
             MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServieceProxy;
         });
 
+    }
+
+    @Test
+    void cglibProxy() {
+        MemberServiceImpl target = new MemberServiceImpl();
+        ProxyFactory proxyFactory = new ProxyFactory(target);
+        proxyFactory.setProxyTargetClass(true); // CGLIB 프록시
+
+        // 프록시를 인터페이스로 캐스팅 성공
+//        Object proxy = proxyFactory.getProxy();
+        MemberService memberServieceProxy = (MemberService) proxyFactory.getProxy();
+
+        log.info("proxy calss = {}", memberServieceProxy.getClass());
+
+        // CGLIB 프록시를 구현 클래스로 캐스팅 시도 성공
+        MemberServiceImpl castingMemberService = (MemberServiceImpl) memberServieceProxy;
     }
 }
